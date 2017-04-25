79d12776186e149e253811e05a999337b70fa458
Joseph <victoryandjoseph@gmail.com>
2017-04-25 22:15:48 +0800

    DESC: add storyboard

--- algorithmPrac/ViewController.m
+++ algorithmPrac/ViewController.m
@@ -17,12 +17,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *testArray=@[@6,@5,@4,@3,@2,@1];
+    NSArray *result=[self forInsertSort:testArray];
+    NSLog(@"result %@",result);
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+-(NSArray*)forInsertSort:(NSArray*)dataArray{
+    NSMutableArray *sortedArray=[[NSMutableArray alloc]initWithArray:dataArray];
+    NSArray *resultArray=sortedArray;
+    
+    for (int i=1; i<sortedArray.count ; i++) {
+        int sortedIndex=i;
+        while (sortedIndex>0 && [sortedArray[sortedIndex]integerValue] < [sortedArray[sortedIndex-1] integerValue] ) {
+            // swap them
+            [sortedArray exchangeObjectAtIndex:sortedIndex withObjectAtIndex:sortedIndex-1];
+            sortedIndex--;
+            NSLog(@"sorted %@",sortedArray);
+        }
+    }
+    return resultArray;
 }
 
 
